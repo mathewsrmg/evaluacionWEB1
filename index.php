37efe249d2ef13cc2e7a180a27e99759fc5fe357
Mathew <mathewsrmg@gmail.com>
2020-10-04 21:11:26 -0500

    update all
    all index are changed in the class nav-link 3 = "pagina otra" CHANGED TO  "pagina zapatos"

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
                        <a class="nav-link" href="index2.php">PAGINA GIMNACIO <span class="sr-only">(current)</span></a>
                    </li>
                    <li class="nav-item">
-                       <a class="nav-link" href="index3.php">PAGINA otra <span class="sr-only">(current)</span></a>
+                       <a class="nav-link" href="index3.php">PAGINA ZAPATOS <span class="sr-only">(current)</span></a>
                    </li>
                    <li class="nav-item">
                        <a class="nav-link" href="index4.php">PAGINA POSTOBON <span class="sr-only">(current)</span></a>
